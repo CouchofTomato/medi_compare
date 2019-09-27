37258078273bd848275655c88026ebd3020ba787
Austin Mason <austinpmason@gmail.com>
2019-09-27 14:28:09 +0100

    Add null constraints to the benefits table
    Originally the Benefit model only enforced the presence of the name and
    category attributes on the model. This adds null constraint checks to
    the database also

--- db/migrate/20190920115440_create_benefits.rb
+++ db/migrate/20190920115440_create_benefits.rb
@@ -1,8 +1,8 @@
 class CreateBenefits < ActiveRecord::Migration[6.0]
   def change
     create_table :benefits do |t|
-      t.string :name
-      t.string :category
+      t.string :name, null: false
+      t.string :category, null: false
 
       t.timestamps
     end
