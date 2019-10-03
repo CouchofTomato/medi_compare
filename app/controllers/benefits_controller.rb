c724d58905e7559d6caa0f1e71380befacb1292d
Austin Mason <austinpmason@gmail.com>
2019-10-03 11:47:06 +0100

    Add benefit index page
    This adds the Benefit index page and controller action.
    
    It still requires the fontawesome edit and delete icons but they will be
    added when the delete action and edit action are written in the
    controller

--- app/controllers/benefits_controller.rb
+++ app/controllers/benefits_controller.rb
@@ -1,2 +1,18 @@
 class BenefitsController < ApplicationController
+  before_action :instantiate_benefit_categories, only: [:index, :create]
+
+  def index
+    @benefits = Benefit.all
+    @benefit = Benefit.new
+  end
+
+  private
+
+  def benefit_params
+    params.require(:benefit).permit(:name, :category)
+  end
+
+  def instantiate_benefit_categories
+    @benefit_categories = Benefit::CATEGORY_NAMES
+  end
 end
