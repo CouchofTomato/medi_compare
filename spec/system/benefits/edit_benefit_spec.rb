15c1537009a29d5c1673264d2f4e09b6a4261071
Austin Mason <austinpmason@gmail.com>
2019-10-18 22:04:11 +0100

    Add system test for editing a benefit
    The system tests that you can update both the name and category as well
    as ensure you cannot update a benefit with an empty name

---
+++ spec/system/benefits/edit_benefit_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'editing a benefit', type: :system do
+  before :example do
+    user = create(:user)
+    login_as(user, scope: :user)
+    Benefit.create(name: 'Accomodation', category: 'inpatient')
+    Benefit.create(name: 'Surgery', category: 'inpatient')
+    Benefit.create(name: 'Surgery', category: 'outpatient')
+    Benefit.create(name: 'Specialist Consultations', category: 'outpatient')
+    Benefit.create(name: 'Physiotherapy', category: 'outpatient')
+  end
+
+  it 'allows a user to edit a benefit name' do
+    visit benefits_path
+    find('#edit-Accomodation-inpatient').click
+    fill_in 'benefit[name]', with: 'Medicines'
+    click_on 'Submit'
+    expect(page).to have_content 'Benefit Successfully Updated'
+    expect(page).to have_content 'Medicines'
+  end
+
+  it 'allows a user to edit a benefit category' do
+    visit benefits_path
+    find('#edit-Accomodation-inpatient').click
+    select 'Outpatient', from: 'benefit[category]'
+    click_on 'Submit'
+    expect(page).to have_content 'Benefit Successfully Updated'
+  end
+
+  it 'doesn\'t allow a user to update a benefit name as an empty string' do
+    visit benefits_path
+    find('#edit-Accomodation-inpatient').click
+    fill_in 'benefit[name]', with: ''
+    click_on 'Submit'
+    expect(page).to have_content 'Benefit could not be updated.'
+    expect(page).to have_content 'Name can\'t be blank'
+  end
+end
