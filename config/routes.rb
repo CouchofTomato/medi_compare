4b0e3a6b5fbe89e00059a0da48c4aee9b3f28a5b
Austin Mason <austinpmason@gmail.com>
2019-10-22 21:50:56 +0100

    create insurers controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   resources :benefits
+  resources :insurers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
