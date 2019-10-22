840c68bf19d668b6568cd7ccebc000bc5d4c08a1
Austin Mason <austinpmason@gmail.com>
2019-10-22 22:39:20 +0100

    Add Insurer search scope function
    The search scope function takes in a search term and returns any
    insurers who match the search term. The match is for any part of the
    string. If no search term is provided then all Insurers are returned

--- app/models/insurer.rb
+++ app/models/insurer.rb
@@ -1,3 +1,11 @@
 class Insurer < ApplicationRecord
   validates :name, presence: true
+
+  scope :search, -> (search_term) {
+    if search_term
+      where("name ILIKE ?", "%#{search_term.squish}%")
+    else
+      Insurer.all
+    end
+  }
 end
--- spec/models/insurer_spec.rb
+++ spec/models/insurer_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Insurer, type: :model do
   it { is_expected.to validate_presence_of :name }
+
+  describe '.search' do
+    let(:search_term) { 'BUP' }
+    let!(:filtered_insurers) {[
+      Insurer.create(name: 'BUPA Global')
+    ]}
+    Insurer.create(name: 'AXA Global')
+    Insurer.create(name: 'Aetna Global Benefits')
+    
+    it 'returns the Insurers whose name or category matches the search term' do
+      expect(Insurer.search(search_term)).to match_array(filtered_insurers)
+    end
+
+    it 'returns all Insurers if the search term is nil' do
+      expect(Insurer.search(nil)).to match_array(Insurer.all)
+    end
+  end
 end
