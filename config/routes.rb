b81c5f53c6956e8f7ddb3a423b62fa298177ccf7
Austin Mason <austinpmason@gmail.com>
2019-11-01 14:05:38 +0000

    Modify product route to make it a resource
    Products had a resources route but products are only even shown as a
    nested resource of an insurer so an index page is not required

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :benefits
   resources :insurers do
-    resources :products
+    resource :products
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
