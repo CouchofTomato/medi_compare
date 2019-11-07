d72c8415de8fbd16180dcb058fb06b266b25a806
Austin Mason <austinpmason@gmail.com>
2019-11-07 15:52:35 +0000

    add product create action in controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,22 @@ class ProductsController < ApplicationController
     @insurer = Insurer.find(params[:insurer_id])
     @product = Product.new
   end
+
+  def create
+    @insurer = Insurer.find(params[:insurer_id])
+    @product = @insurer.products.build(product_params)
+    if @product.save
+      flash[:notice] = 'Product created'
+      redirect_to insurer_path(@insurer)
+    else
+      flash.now[:warning] = 'Product could not be created'
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
 end
