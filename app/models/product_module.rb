2abfd26806c3f62c32d281fbaaeea25974c164eb
Austin Mason <austinpmason@gmail.com>
2019-11-12 15:32:30 +0000

    Add Product Module validations
    This validates both name and category are present in product module and
    adds the necessary tests to the spec file confirming this.

--- app/models/product_module.rb
+++ app/models/product_module.rb
@@ -1,3 +1,17 @@
 class ProductModule < ApplicationRecord
+  CATEGORY_NAMES =
+    %w[
+      core
+      outpatient
+      medicines_and_appliances
+      wellness
+      maternity
+      dental & optical
+      evacuation_and_repatriation
+    ].freeze
+
+  validates :name, presence: true
+  validates :category, presence: true, inclusion: CATEGORY_NAMES
+
   belongs_to :product
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_135556) do
+ActiveRecord::Schema.define(version: 2019_11_12_144231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2019_10_31_135556) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "product_modules", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "sum_assured"
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_product_modules_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -64,5 +74,6 @@ ActiveRecord::Schema.define(version: 2019_10_31_135556) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "product_modules", "products"
   add_foreign_key "products", "insurers"
 end
