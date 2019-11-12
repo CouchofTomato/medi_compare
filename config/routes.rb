060ae628f9dc0250e44561ca89a8b56a2566c51c
Austin Mason <austinpmason@gmail.com>
2019-11-12 15:44:45 +0000

    add routes for product_modules

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   resources :insurers do
     resources :products
   end
+
+  resources :products do
+    resources :product_modules
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
