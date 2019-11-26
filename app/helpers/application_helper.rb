e421e4b6379ee4cfa172ff3d88bc66867d093a70
Austin Mason <austinpmason@gmail.com>
2019-11-26 16:15:08 +0000

    add benefits select box to new product_module page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def group_by_category(input)
     input.group_by(&:category)
   end
+
+  def options_for_benefits_select(input)
+    group_by_category(input).map do |category, benefits|
+      [category.titleize, benefits.map { |benefit| [benefit.name.titleize, benefit.id] }]
+    end.to_h
+  end
 end
