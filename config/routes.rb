0a0964000236489bb219f97608d7a7ad1f26041e
Austin Mason <austinpmason@gmail.com>
2019-12-09 14:30:10 +0000

    add product_modules standalone route

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   resources :products do
     resources :product_modules
   end
+
+  resources :product_modules
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
