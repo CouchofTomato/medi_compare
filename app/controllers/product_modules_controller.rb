9cffc6bae07cc3e2ce70a3011f8a4a3ce18688c8
Austin Mason <austinpmason@gmail.com>
2019-12-09 14:32:15 +0000

    add product_module_categories to ProductModules controller new method

--- app/controllers/product_modules_controller.rb
+++ app/controllers/product_modules_controller.rb
@@ -3,5 +3,6 @@ class ProductModulesController < ApplicationController
     @product = Product.find(params[:product_id])
     @product_module = @product.product_modules.new
     @benefits = Benefit.all
+    @product_module_categories = ProductModule::CATEGORY_NAMES
   end
 end
