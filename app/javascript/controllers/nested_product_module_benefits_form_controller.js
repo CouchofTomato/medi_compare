eb5ed888136e9bcbeea52b9bc4d9a47068ae1379
Austin Mason <austinpmason@gmail.com>
2020-01-09 21:16:35 +0000

    refactor nested product module benefits controller

--- app/javascript/controllers/nested_product_module_benefits_form_controller.js
+++ app/javascript/controllers/nested_product_module_benefits_form_controller.js
@@ -10,11 +10,10 @@ export default class extends Controller {
       return {benefit_id: benefit.value, name: benefit.textContent}
     })
     const benefitHTML = benefitSelection.map(benefit => {
-      let content = this.templateTarget.innerHTML
+      return this.templateTarget.innerHTML
         .replace(/TEMPLATE_RECORD/g, new Date().valueOf())
         .replace(/BENEFIT_ID/g, benefit.benefit_id)
         .replace(/BENEFIT_NAME/g, benefit.name)
-      return content
     })
     this.templatePlaceholderTarget.insertAdjacentHTML('beforebegin', benefitHTML.join(' '))
   }
