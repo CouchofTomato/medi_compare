085468733a863cb7ae043b2d4b29fec350e72f4d
Austin Mason <austinpmason@gmail.com>
2020-01-09 21:42:27 +0000

    create generateTemplateHTML function in nested-product-module-benefits stimulus controller to refactor addBenefits function

--- app/javascript/controllers/nested_product_module_benefits_form_controller.js
+++ app/javascript/controllers/nested_product_module_benefits_form_controller.js
@@ -6,12 +6,16 @@ export default class extends Controller {
   addBenefits(event) {
     event.preventDefault()
     const selectedBenefits = Array.from(document.querySelectorAll('#benefits option:checked'))
-    const benefitHTML = selectedBenefits.map(benefit => {
+    const benefitHTML = this.generateTemplateHTML(selectedBenefits)
+    this.templatePlaceholderTarget.insertAdjacentHTML('beforebegin', benefitHTML)
+  }
+
+  generateTemplateHTML(benefits) {
+    return benefits.map(benefit => {
       return this.templateTarget.innerHTML
         .replace(/TEMPLATE_RECORD/g, new Date().valueOf())
         .replace(/BENEFIT_ID/g, benefit.value)
         .replace(/BENEFIT_NAME/g, benefit.textContent)
-    })
-    this.templatePlaceholderTarget.insertAdjacentHTML('beforebegin', benefitHTML.join(' '))
+    }).join(' ')
   }
 }
