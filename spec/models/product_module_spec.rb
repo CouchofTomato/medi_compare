d0f57f341cb60a42045fee81ba42ce9195f6cf80
Austin Mason <austinpmason@gmail.com>
2020-01-09 21:49:48 +0000

    add has_many product_module_benefits association to product module model

--- spec/models/product_module_spec.rb
+++ spec/models/product_module_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe ProductModule, type: :model do
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :category }
   it { is_expected.to validate_inclusion_of(:category).in_array ProductModule::CATEGORY_NAMES }
+  it { is_expected.to have_many(:product_module_benefits) }
+  it { is_expected.to have_many(:benefits).through(:product_module_benefits) }
 end
