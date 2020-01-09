6100a8a6993e7ae5b734da87e4b65a818de5bb7a
Austin Mason <austinpmason@gmail.com>
2020-01-09 21:25:34 +0000

    remove redundant map from addBenefit function in nested product module benefits stimulus controller

--- app/javascript/controllers/nested_product_module_benefits_form_controller.js
+++ app/javascript/controllers/nested_product_module_benefits_form_controller.js
@@ -6,14 +6,11 @@ export default class extends Controller {
   add_benefits(event) {
     event.preventDefault()
     const selectedBenefits = Array.from(document.querySelectorAll('#benefits option:checked'))
-    const benefitSelection = selectedBenefits.map(benefit => {
-      return {benefit_id: benefit.value, name: benefit.textContent}
-    })
-    const benefitHTML = benefitSelection.map(benefit => {
+    const benefitHTML = selectedBenefits.map(benefit => {
       return this.templateTarget.innerHTML
         .replace(/TEMPLATE_RECORD/g, new Date().valueOf())
-        .replace(/BENEFIT_ID/g, benefit.benefit_id)
-        .replace(/BENEFIT_NAME/g, benefit.name)
+        .replace(/BENEFIT_ID/g, benefit.value)
+        .replace(/BENEFIT_NAME/g, benefit.textContent)
     })
     this.templatePlaceholderTarget.insertAdjacentHTML('beforebegin', benefitHTML.join(' '))
   }
