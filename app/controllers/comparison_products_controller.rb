5960077772bb037f198debea3767e9ba98d97c22
Austin Mason <austinpmason@gmail.com>
2020-03-01 17:07:08 +0000

    Remove ComparisonProduct model
    The model has been refactored out as the logic is small enough to live
    in the controller

--- app/controllers/comparison_products_controller.rb
+++ app/controllers/comparison_products_controller.rb
@@ -1,6 +1,12 @@
 class ComparisonProductsController < ApplicationController
   def create
-    @comparison_product = ComparisonProduct.new(comparison_product_params)
+    @selected_product = Insurer.includes(
+      products: { product_modules: { product_module_benefits: :benefit } }
+    ).where(
+      id: params[:insurer],
+      products: { id: params[:product], product_modules: selected_modules(params[:product_modules]) }
+    )
+
     respond_to do |format|
       format.js
     end
@@ -12,6 +18,13 @@ class ComparisonProductsController < ApplicationController
 
   def comparison_product_params
     params.require(:comparison_product)
-          .permit(:insurer, :product, :product_modules)
+          .permit(:insurer, :product,
+                  product_modules: %i[core outpatient medicines_and_appliances
+                                      wellness maternity dental_and_optical
+                                      evacuation_and_repatriation])
+  end
+
+  def selected_modules(modules)
+    modules.values.flatten
   end
 end
--- app/models/comparison_product.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ComparisonProduct
-  include ActiveModel::Model
-
-  attr_reader :insurer, :product, :product_modules
-end
